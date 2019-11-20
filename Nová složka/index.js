cf9c86e4a80124489f76dbdf5e7cd6a6488616db
lukascer <lukas.cernohous@email.cz>
2019-11-20 17:44:48 +0100

    readme update

---
+++ Nová složka/index.js
@@ -0,0 +1,11 @@
+let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AP', 'AQ', 'AR', 'AS', 'AT', 'AU', 'AV', 'AW', 'AX', 'AY', 'AZ']
+
+const calcAlphabetFindPos = (letter) => { return alphabet.indexOf(letter) }
+const calcAlphabetFindLetter = (number) => { return alphabet[number] }
+
+module.exports = {
+    calcAlphabetFindPos: calcAlphabetFindPos,
+    findPosition: calcAlphabetFindPos,
+    calcAlphabetFindLetter: calcAlphabetFindLetter,
+    findLetter: calcAlphabetFindLetter
+}
